1a130e8f8b777e9762cf24ae8847b5a635e3aefa
jamooooong <102500033+jamooooong@users.noreply.github.com>
2024-09-24 13:35:55 +0900

    ✨ feat: update server.js with corrected ranking value

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ app.get('/badge', async (req, res) => {
         const userData = await getUserRank(name);
         
         // API 응답에서 langName 추출
-        const lang = userData.langName || 'Unknown';
-        const score = '123,456점';
-        const ranking = '1444등등등';
-
-        const svg = generateSVG(name, lang, score, ranking);
-
+        const langName =  '으으으';
+        const score = '스스스';
+        const userRank = '등등등';
+        const grade = 'yellow';
+        const svg = generateSVG(name, langName, score, userRank, grade);
+        
         // camo 캐싱 방지를 위한 헤더 설정
         res.setHeader('Cache-Control', 'max-age=0, no-cache, no-store, must-revalidate');
         res.setHeader('Pragma', 'no-cache');
